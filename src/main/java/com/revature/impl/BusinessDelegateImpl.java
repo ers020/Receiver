a38bad9fd88380d5a044c3027da7c9b3df4ebd0a
Eric <mr.eric.erwin@gmail.com>
2016-09-27 14:45:52 -0400

    Spring beans xml update

--- src/main/java/com/revature/impl/BusinessDelegateImpl.java
+++ src/main/java/com/revature/impl/BusinessDelegateImpl.java
@@ -1,7 +1,20 @@
 package com.revature.impl;
 
 import com.revature.data.BusinessDelegate;
+import com.revature.data.DataService;
 
 public class BusinessDelegateImpl implements BusinessDelegate {
 
+	private DataService dataService;
+
+	public DataService getDataService() {
+		return dataService;
+	}
+
+	public void setDataService(DataService dataService) {
+		this.dataService = dataService;
+	}
+	
+	
+	
 }
--- src/main/java/com/revature/impl/DataServiceImpl.java
+++ src/main/java/com/revature/impl/DataServiceImpl.java
@@ -1,7 +1,20 @@
 package com.revature.impl;
 
+import com.revature.data.DAO;
 import com.revature.data.DataService;
 
 public class DataServiceImpl implements DataService {
 
+	private DAO dao;
+
+	public DAO getDao() {
+		return dao;
+	}
+
+	public void setDao(DAO dao) {
+		this.dao = dao;
+	}
+	
+	
+	
 }
