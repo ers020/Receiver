bd521dc4373a2a7c414d4444dbf48b79f5f14671
Eric <mr.eric.erwin@gmail.com>
2016-09-30 14:40:40 -0400

    some styling complete

--- src/main/webapp/WEB-INF/resources/js/controllers/AuditCtrl.js
+++ src/main/webapp/WEB-INF/resources/js/controllers/AuditCtrl.js
@@ -6,9 +6,7 @@ audits.controller("AuditCtrl", ["$scope", "$http", function($scope, $http){
 	
 	$http.get("http://localhost:7001/Receiver/api/audits").success(
 			function(resp){
-				console.log("It looks like we made it.");
 				$scope.audits = resp;
-				console.log(resp);
 				$("#loading").hide();
 				
 				
